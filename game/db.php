9006154010913a933131c3ce9789546f0eceea09
dauzer <rybachuk.iaroslav@gmail.com>
2020-04-14 20:08:37 +0300

    ia

--- game/db.php
+++ game/db.php
@@ -1,40 +1,40 @@
 <?php
 $host = 'localhost';
-$usname = 'Mishanya';
-$pass = 'cocosderty';
+$usname = 'root';
+$pass = '';
 $table = 'users';
 try{
 $pdo = new PDO(
     "mysql:host=$host;dbname=test",
     $usname,
-    $pass,
+    $pass
 );
 }catch(PDOException $e) {
     print "Error!: " . $e->getMessage() . "<br/>";
     die();
 }
-    try{
-        $sql = <<<EOSQL
-            CREATE TABLE IF NOT EXISTS users (
-                user_id     INT AUTO_INCREMENT   PRIMARY KEY,
-                username    VARCHAR (100)        NOT NULL,
-                hp          INT (10)             DEFAULT NULL,
-                password    VARCHAR (100)        NOT NULL,
-            );
-    EOSQL;
-        $pdo ->exec($sql);
-        var_dump($pdo);
-    }catch(PDOException $e){
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
 
-function getHp($user_iD, $pdo){
-    $sql = "SELECT `hp` FROM `users` WHERE $user_iD";
-    $res = $pdo->query("SELECT `hp` FROM `users` WHERE `user_id` = $user_iD");
-    $l_records = $res->fetch(PDO::FETCH_ASSOC);
-    return $l_records['hp'];
+try{
+$sql = "CREATE TABLE employees (
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    user_name VARCHAR(30) NOT NULL,
+    password VARCHAR(30) NOT NULL,
+    hp VARCHAR(50),
+    )";
+// use exec() because no results are returned
+$pdo->exec($sql);
+echo "Table employees created successfully";
 }
+catch(PDOException $e)
+    {
+        echo $sql . "<br>" . $e->getMessage();
+    }
+//function getHp($user_iD, $pdo){
+//    $sql = "SELECT `hp` FROM `users` WHERE $user_iD";
+//    $res = $pdo->query("SELECT `hp` FROM `users` WHERE `user_id` = $user_iD");
+//    $l_records = $res->fetch(PDO::FETCH_ASSOC);
+//    return $l_records['hp'];
+//}
 try{
     function updHp($user_iD, $pdo, $newHp){
         $sql = "UPDATE `users` SET `hp`=$newHp WHERE `user_id`= $user_iD";
